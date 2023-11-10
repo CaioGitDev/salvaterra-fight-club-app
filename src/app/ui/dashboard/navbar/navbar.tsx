b4123c1a2e113de01cf2fc0373464ea42fbc2b6c
CaioDev <53819717+CaioGitDev@users.noreply.github.com>
2023-11-10 17:49:21 +0000

    feat: add navbar html

--- src/app/ui/dashboard/navbar/navbar.tsx
+++ src/app/ui/dashboard/navbar/navbar.tsx
@@ -1,5 +1,38 @@
+"use client"
+
+import { usePathname } from 'next/navigation'
+import styles from './navbar.module.css'
+import { MdNotifications, MdOutlineChat, MdPublic, MdSearch } from 'react-icons/md'
+import Image from 'next/image'
+
+import userImage from '../../../../../public/images/caio.svg'
+
 const Navbar = () => {
-  return <div>Navbar</div>
+  const pathName = usePathname()
+  return (
+    <div className={styles.container}>
+      <div className={styles.title}>{pathName.split('/').pop()}</div>
+
+      <div className={styles.menu}>
+        <div className={styles.search}>
+          <MdSearch />
+          <input type="text" placeholder='Search...' className={styles.input} />
+        </div>
+        <div className={styles.user}>
+        <Image className={styles.user_image} src={userImage} alt="" width={50} height={50} />
+        <div className={styles.user_detail}>
+          <span className={styles.user_name}>Salvaterra Fight Club</span>
+          <span className={styles.user_description}>Artes Marciais e Desportos de Combate</span>
+        </div>
+      </div>
+        <div className={styles.icons}>
+          <MdOutlineChat size={20} />
+          <MdNotifications size={20} />
+          <MdPublic size={20} />
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Navbar
