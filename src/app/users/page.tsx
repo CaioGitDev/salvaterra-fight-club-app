d206ce5f105a42a4fce341ea99ffeef30623bff4
CaioDev <53819717+CaioGitDev@users.noreply.github.com>
2023-11-15 17:42:32 +0000

    feat: create dx custom theme metadata file

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -1,8 +1,21 @@
+'use client'
 import Form from '@/app/ui/users/form/form'
 import styles from '@/app/ui/users/users.module.css'
 
+import 'devextreme/dist/css/dx.salvaterra-fight-club-theme.min.css'
+import Button from 'devextreme-react/button'
+
 const UsersPage = () => {
-  return <div className={styles.container}></div>
+  function sayHelloWorld() {
+    alert('Hello world!')
+  }
+  return (
+    <div suppressHydrationWarning className="dx-viewport p-5">
+      <div className={styles.container}>
+        <Button text="Click me" type="success" onClick={sayHelloWorld} />
+      </div>
+    </div>
+  )
 }
 
 export default UsersPage
