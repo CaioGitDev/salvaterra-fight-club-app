8de625f4bfd47b69e36d856dfc7b3320899899b5
Caio Rosa <dev.caiorosa@gmail.com>
2023-11-27 22:31:00 +0000

    feat: change datagrid method build

--- src/app/members/page.tsx
+++ src/app/members/page.tsx
@@ -2,15 +2,13 @@ import styles from '@/app/ui/members/members.module.css'
 
 import MembersDataGrid from '../ui/members/membersDataGrid/membersDataGrid'
 
-const MembersPage = () => {
-  return (
-    <div className="p-5">
-      <h4 className="pb-5"></h4>
-      <div className={styles.container}>
-        <MembersDataGrid />
-      </div>
+const MembersPage = () => (
+  <div className="p-5">
+    <h4 className="pb-5"></h4>
+    <div className={styles.container}>
+      <MembersDataGrid />
     </div>
-  )
-}
+  </div>
+)
 
 export default MembersPage
--- src/app/ui/members/membersDataGrid/membersDataGrid.tsx
+++ src/app/ui/members/membersDataGrid/membersDataGrid.tsx
@@ -3,28 +3,29 @@ import '@/app/ui/theme/dx.material.salvaterra-fight-club-theme.css'
 import { serviceData } from '@/lib/member-datagrid-seed'
 
 import DataGrid, {
-  Editing,
   FilterRow,
   GroupPanel,
   HeaderFilter,
   Scrolling,
-  Popup,
-  Form,
-  PatternRule,
+  LoadPanel,
+  SearchPanel,
+  Export,
+  Toolbar,
+  Item,
+  DataGridTypes,
 } from 'devextreme-react/data-grid'
 
-import {
-  EmailRule,
-  Item,
-  RequiredRule,
-  StringLengthRule,
-} from 'devextreme-react/form'
 import config from 'devextreme/core/config'
 
 import getColumnsDefinition from '@/lib/devExtreme/member-datagrid-columns-definition'
-import { useEffect, useRef, useState } from 'react'
+import { useCallback, useEffect, useRef, useState } from 'react'
 import { MemberDataInterface } from '@/lib/definitions'
 import DataSource from 'devextreme/data/data_source'
+import CustomStore from 'devextreme/data/custom_store'
+import Button from 'devextreme-react/button'
+import Popup from 'devextreme-react/popup'
+import MembersPopup from '@/components/members-popup/membersPopup'
+import notify from 'devextreme/ui/notify'
 
 const datepickOptions = {
   displayFormat: 'yyyy-MM-dd',
@@ -52,14 +53,53 @@ config({
   editorStylingMode: 'underlined',
 })
 
+const exportFormats = ['xlsx', 'pdf']
+
 const MembersDataGrid = () => {
   const [gridDataSource, setGridDataSource] =
-    useState<DataSource<MemberDataInterface, string>>()
+    useState<CustomStore<MemberDataInterface, string>>()
+  const [isPanelOpened, setPanelOpened] = useState(false)
+  const [memberId, setMemberId] = useState<string>('')
+  const [popupVisible, setPopupVisible] = useState(false)
+  const [formDataDefaults, setFormDataDefaults] = useState('')
   const gridRef = useRef<DataGrid>(null)
+
+  const handleNewMemberClick = useCallback(() => {
+    console.log('handleNewMemberClick')
+    setPopupVisible(true)
+  }, [])
+
+  const handleRefreshClick = useCallback(() => {
+    gridRef.current?.instance.refresh()
+  }, [])
+
+  const handleOnRowClick = useCallback(
+    ({ data }: DataGridTypes.RowClickEvent) => {
+      setMemberId(data.id)
+      setPanelOpened(true)
+    },
+    [],
+  )
+
+  const handleOnSaveClick = useCallback(() => {
+    notify(
+      {
+        message: `Novo Membro criado com sucesso`,
+        position: { at: 'bottom center', my: 'bottom center' },
+      },
+      'success',
+    )
+
+    setPopupVisible(false)
+  }, [])
+
+  const changePopupVisibility = useCallback((isVisble: boolean) => {
+    setPopupVisible(isVisble)
+  }, [])
 
   useEffect(() => {
     setGridDataSource(
-      new DataSource({
+      new CustomStore({
         key: 'member.id',
         load: () => serviceData.getMembers(),
       }),
@@ -73,294 +113,58 @@ const MembersDataGrid = () => {
         showBorders={true}
         height={700}
         defaultColumns={getColumnsDefinition(serviceData)}
+        onRowClick={handleOnRowClick}
         ref={gridRef}
       >
+        <LoadPanel showPane={false} />
+        <SearchPanel visible width={200} placeholder="Pesquisa de Membro" />
+        <Export enabled allowExportSelectedData formats={exportFormats} />
         <FilterRow visible={true} />
         <HeaderFilter visible={true} />
         <GroupPanel visible={true} />
         <Scrolling mode="virtual" />
-        <Editing
-          mode="popup"
-          useIcons={true}
-          allowAdding={true}
-          allowUpdating={true}
-          allowDeleting={true}
-        >
-          <Popup title="Membro" showTitle={true} width="90vw" height="80vh" />
-          <Form showValidationSummary={true}>
-            {/* identificação */}
-            <Item
-              itemType="group"
-              caption="Identificação"
-              colCount={2}
-              colSpan={2}
-            >
-              <Item
-                isRequired={true}
-                dataField="member.fullName"
-                label={{ text: 'Nome Completo', location: 'top' }}
-              >
-                <RequiredRule message="Nome is required" />
-                <PatternRule
-                  message="Do not use digits in the Name"
-                  pattern={/^[^0-9]+$/}
-                />
-              </Item>
-              <Item
-                isRequired={true}
-                dataField="member.gender_id"
-                label={{ text: 'Genero', location: 'top' }}
-                editorType="dxSelectBox"
-                editorOptions={{ dataSource: serviceData.getGengerList() }}
-              />
-              <Item
-                isRequired={true}
-                dataField="member.dateOfBirth"
-                editorType="dxDateBox"
-                editorOptions={{ ...datepickOptions, max: new Date() }}
-                label={{ text: 'Data Nascimento', location: 'top' }}
-              />
-              <Item
-                isRequired={true}
-                dataField="member.nationality_id"
-                label={{ text: 'Nacionalidade', location: 'top' }}
-                editorType="dxSelectBox"
-                editorOptions={{
-                  dataSource: serviceData.getNacionalitiesList(),
-                }}
-              />
-              <Item
-                isRequired={true}
-                dataField="member.placeOfBirth"
-                label={{ text: 'Naturalidade', location: 'top' }}
-              ></Item>
-              <Item
-                dataField="member.email"
-                label={{ text: 'Email', location: 'top' }}
-              >
-                <RequiredRule message="Email is required" />
-                <EmailRule message="Email is invalid" />
-              </Item>
-              <Item
-                isRequired={true}
-                dataField="member.contact"
-                label={{ text: 'Nº Telemóvel', location: 'top' }}
-                editorOptions={phoneNumberOptions}
-              ></Item>
-            </Item>
-            {/* dados morada */}
-            <Item itemType="group" caption="Morada" colCount={2} colSpan={2}>
-              <Item
-                isRequired={true}
-                dataField="memberAddress.address"
-                label={{ text: 'Morada', location: 'top' }}
-              />
-              <Item
-                isRequired={true}
-                dataField="memberAddress.city"
-                label={{ text: 'Cidade', location: 'top' }}
-              />
-              <Item
-                isRequired={true}
-                dataField="memberAddress.county"
-                label={{ text: 'Concelho', location: 'top' }}
-              />
-              <Item
-                isRequired={true}
-                dataField="memberAddress.parish"
-                label={{ text: 'Freguesia', location: 'top' }}
-              />
-              <Item
-                isRequired={true}
-                dataField="memberAddress.postalCode"
-                label={{ text: 'Cod. Postal', location: 'top' }}
-                editorOptions={postalCodeOptions}
-              ></Item>
-            </Item>
-            {/* identificação fiscal */}
-            <Item
-              itemType="group"
-              caption="Documento de Identificação"
-              colCount={2}
-              colSpan={2}
-            >
-              <Item
-                isRequired={true}
-                dataField="memberIdentificationDocument.identificationDocument_id"
-                label={{
-                  text: 'Documento de identificação',
-                  location: 'top',
-                }}
-                editorType="dxSelectBox"
-                editorOptions={{
-                  dataSource: serviceData.getIdentificationDocumentList(),
-                }}
-              />
-              <Item
-                isRequired={true}
-                dataField="memberIdentificationDocument.identificationNumber"
-                label={{ text: 'Nº Identificação', location: 'top' }}
-              />
-              <Item
-                isRequired={true}
-                dataField="memberIdentificationDocument.expireDate"
-                label={{ text: 'Data de Validade', location: 'top' }}
-                editorType="dxDateBox"
-                editorOptions={{ ...datepickOptions, min: new Date() }}
-              />
-              <Item
-                isRequired={true}
-                dataField="memberIdentificationDocument.taxIdentificationNumber"
-                label={{ text: 'Nº Identificação Fiscal', location: 'top' }}
-                editorType="dxNumberBox"
-                editorOptions={{
-                  value: null,
-                }}
-              >
-                <StringLengthRule
-                  message="NIF deve conter 9 digitos"
-                  min={9}
-                  max={9}
-                />
-              </Item>
-            </Item>
-            {/* responsavel */}
-            <Item
-              itemType="group"
-              caption="Responsável"
-              colCount={2}
-              colSpan={2}
-            >
-              <Item
-                dataField="memberGuardian.fullName"
-                label={{ text: 'Nome Responsável', location: 'top' }}
-              />
-              <Item
-                dataField="memberGuardian.contact"
-                label={{ text: 'Nº Telemóvel', location: 'top' }}
-                editorOptions={phoneNumberOptions}
-              />
-              <Item
-                dataField="memberGuardian.relationshipDegree_id"
-                label={{ text: 'Grau de Parentesco', location: 'top' }}
-                editorType="dxSelectBox"
-                editorOptions={{
-                  dataSource: serviceData.getRelationshipDegreeListList(),
-                }}
-              />
-              <Item
-                dataField="memberGuardian.address"
-                label={{ text: 'Morada', location: 'top' }}
-              />
-              <Item
-                dataField="memberGuardian.city"
-                label={{ text: 'Cidade', location: 'top' }}
-              />
-              <Item
-                dataField="memberGuardian.county"
-                label={{ text: 'Concelho', location: 'top' }}
-              />
-              <Item
-                dataField="memberGuardian.parish"
-                label={{ text: 'Freguesia', location: 'top' }}
-              />
-              <Item
-                dataField="memberGuardian.postalCode"
-                label={{ text: 'Cod. Postal', location: 'top' }}
-                editorOptions={postalCodeOptions}
-              />
-            </Item>
-            {/* Modalidade & Frequência */}
-            <Item
-              itemType="group"
-              caption="Modalidade, Frequência e Pagamentos"
-              colCount={2}
-              colSpan={2}
-            >
-              <Item
-                isRequired={true}
-                dataField="member.modality_id"
-                label={{
-                  text: 'Modalidades',
-                  location: 'top',
-                }}
-                editorType="dxSelectBox"
-                editorOptions={{
-                  dataSource: serviceData.getModalitiesList(),
-                }}
-              />
-              <Item
-                isRequired={true}
-                dataField="member.frequency_id"
-                label={{
-                  text: 'Frequência',
-                  location: 'top',
-                }}
-                editorType="dxSelectBox"
-                editorOptions={{
-                  dataSource: serviceData.getFrequenciesList(),
-                }}
-              />
-              <Item
-                isRequired={true}
-                dataField="member.memberType_id"
-                label={{
-                  text: 'Tipo de inscrição',
-                  location: 'top',
-                }}
-                editorType="dxSelectBox"
-                editorOptions={{
-                  dataSource: serviceData.getMemeberTypesList(),
-                }}
-              />
-              <Item
-                isRequired={true}
-                dataField="member.paymentFrequency_id"
-                label={{
-                  text: 'Periodicidade de pagamentos',
-                  location: 'top',
-                }}
-                editorType="dxSelectBox"
-                editorOptions={{
-                  dataSource: serviceData.getPaymentFrequencyList(),
-                }}
-              />
-            </Item>
-            {/* Termos e condições */}
-            <Item
-              itemType="group"
-              caption="Termos e Condições"
-              colCount={2}
-              colSpan={2}
-            >
-              <Item
-                isRequired={true}
-                dataField="member.termsAndConditions"
-                label={{
-                  text: 'Eu li e concorco com os Termos e Condições assim como o Regulamento interno',
-                  location: 'left',
-                }}
-                editorType="dxCheckBox"
-                editorOptions={{ value: false }}
-                colCount={2}
-                colSpan={2}
-              />
-              <Item
-                isRequired={true}
-                dataField="member.healthDeclaration"
-                label={{
-                  text: 'Declaro dispor da robustez física e que não tenho nenhum problema de saúde que impeça a pratica de atividade física',
-                  location: 'left',
-                }}
-                editorType="dxCheckBox"
-                editorOptions={{ value: false }}
-                colCount={2}
-                colSpan={2}
-              />
-            </Item>
-          </Form>
-        </Editing>
+        <Toolbar>
+          <Item location="before">
+            <div className="grid-header">Lista de Membros</div>
+          </Item>
+          <Item location="after" locateInMenu="auto">
+            <Button
+              icon="plus"
+              text="Novo Membro"
+              type="default"
+              stylingMode="contained"
+              onClick={handleNewMemberClick}
+            />
+          </Item>
+          <Item
+            location="after"
+            locateInMenu="auto"
+            showText="inMenu"
+            widget="dxButton"
+          >
+            <Button
+              icon="refresh"
+              text="Refresh"
+              stylingMode="text"
+              onClick={handleRefreshClick}
+            />
+          </Item>
+          <Item location="after" locateInMenu="auto">
+            <div className="separator" />
+          </Item>
+          <Item name="exportButton" />
+          <Item location="after" locateInMenu="auto">
+            <div className="separator" />
+          </Item>
+          <Item name="searchPanel" locateInMenu="auto" />
+        </Toolbar>
       </DataGrid>
+      <MembersPopup
+        title="Novo Membro"
+        visible={popupVisible}
+        setVisible={changePopupVisibility}
+        onSave={handleOnSaveClick}
+      ></MembersPopup>
     </div>
   )
 }
